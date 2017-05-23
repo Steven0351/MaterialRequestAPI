3e40a17f195cd9faa4c5be3be0ec2d50df078d11
Steven Sherry <steven.sherry@gdt.com>
2017-05-23 08:25:12 -0500

    Added approval boolean and purchase order number field for updating for better visibility for the requestor

--- src/model/purchase-request.js
+++ src/model/purchase-request.js
@@ -3,12 +3,15 @@ import User from './user';
 import InventoryItem from './inventory-item';
 let Schema = mongoose.Schema;
 
+// TODO - Figure out how to utilize storage and CRUD attachments for attached GDT-MFG PO's and Customer PO's
 let PurchaseRequestSchema = new Schema({
     itemsToBePurchased: [{type: Schema.Types.ObjectId, ref: 'InventoryItem'}],
     shippingMethod: String,
     isHot: Boolean,
+    orderHasBeenApproved: Boolean,
     orderHasBeenPlaced: Boolean,
     orderAcknowledgementReceived: Boolean,
+    purchaseOrderNumber: String,
     trackingInformation: String,
     requestor: {
       type: Schema.Types.ObjectId,
