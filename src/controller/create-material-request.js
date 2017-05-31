dc41edb70109f477e4c8cf567c81ea15ca4d4d22
stevensherry0351 <steven.r.sherry@gmail.com>
2017-05-31 05:42:46 -0500

    added more CRUD funcationality for create-material-request controller

--- src/controller/create-material-request.js
+++ src/controller/create-material-request.js
@@ -22,6 +22,43 @@ export default({ config, db }) => {
     });
   });
 
+  api.put('/:id', authenticate, (req, res) => {
+    CreateMaterialRequest.findById(req.params.id, (err, createMaterialRequest) => {
+      if (err) {
+        res.send(err);
+      } else if (req.body.requestor != createMaterialRequest.requestor || req.body.requestor != '5920befd422aeb963bf0fee0') {
+        res.json({message: 'You do not have permission to edit this request'});
+        return;
+      } else {
+        createMaterialRequest.manufacturerSKU = req.body.manufacturerSKU;
+        createMaterialRequest.description = req.body.description;
+        createMaterialRequest.save((err) => {
+          if (err) {
+            res.send(err);
+          }
+          res.json({message: 'Create Material Request successfully update'});
+        });
+      }
+    });
+  });
+
+  api.delete('/:id', authenticate, (req, res) => {
+    CreateMaterialRequest.findById(req.params.id, (err, createMaterialRequest) => {
+      if (err) {
+        res.send(err);
+      } else if (req.body.requestor != createMaterialRequest.requestor || req.body.requestor != '5920befd422aeb963bf0fee0') {
+        res.json({message: 'You do not have permission to edit this request'});
+        return;
+      } else {
+        createMaterialRequest.remove((err) => {
+          if (err) {
+            res.send(err);
+          }
+          res.json({message: 'Create Material Request successfully deleted'});
+        });
+      }
+  });
+
   // 'v1/create-material-request' - Read
   api.get('/', authenticate, (req, res) => {
     CreateMaterialRequest.find({}, (err, createMaterialRequests) => {
