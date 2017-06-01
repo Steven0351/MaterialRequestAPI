b6eec32e0bb083d8fe6e21191fff903d0649f5a0
Steven Sherry <steven.sherry@gdt.com>
2017-06-01 10:56:48 -0500

    added routes for adjustment-request and work-order-completion-request

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import config from '../config';
 import middleware from '../middleware';
 import initializeDb from '../db';
 // Still need to add source file imports for controller files
+import adjustmentrequest from '../controller/adjustment-request';
 import bomrequest from '../controller/bom-request';
 import creatematerialrequest from '../controller/create-material-request';
 import cyclecountrequest from '../controller/cycle-count-request';
 import materialissuerequest from '../controller/material-issue-request';
 import purchaserequest from '../controller/purchase-request';
 import user from '../controller/user';
+import workordercompletionrequest from '../controller/work-order-completion-request';
 
 
 let router = express();
@@ -19,12 +21,14 @@ initializeDb(db => {
   // internal middleware
   router.use(middleware({ config, db }));
   // api routes v1 
+  router.use('/adjustment-request', adjustmentrequest({ config, db}));
   router.use('/bom-request', bomrequest({ config, db }));
   router.use('/create-material-request', creatematerialrequest({ config, db }));
   router.use('/cycle-count-request', cyclecountrequest({ config, db }));
   router.use('/material-issue-request', materialissuerequest({ config, db }));
   router.use('/purchase-request', purchaserequest({ config, db }));
   router.use('/user', user({ config, db }));
+  router.use('/work-order-completion-request', workordercompletionrequest({ config, db}));
 });
 
 export default router;
